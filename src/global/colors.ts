83cfd30e03c59f23cda4a21b0ad9a665ddc83355
iurybrasileiro <iury.brasileiro@outlook.com.br>
2020-12-24 18:58:18 -0300

    chore: add colors file

---
+++ src/global/colors.ts
@@ -0,0 +1,15 @@
+const firefly = '#091227';
+const xumthor = '#EAF0FF';
+
+const colors = {
+  light: {
+    primary: xumthor,
+    secondary: firefly,
+  },
+  dark: {
+    primary: firefly,
+    secondary: xumthor,
+  },
+};
+
+export default colors;
