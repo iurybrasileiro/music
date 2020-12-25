daebdd3532328064f29c36d7acdcf708a3af59c8
iurybrasileiro <iury.brasileiro@outlook.com.br>
2020-12-25 20:01:33 -0300

    chore: add strings file

---
+++ src/global/strings.ts
@@ -0,0 +1,11 @@
+export default {
+  recommented_for_you: 'Recommended for you',
+  my_playlist: 'My Playlist',
+  liked_songs: 'Liked Songs',
+  playing_now: 'Playing Now',
+  profile: 'Profile',
+  language: 'Language',
+  contact_us: 'Contact us',
+  faqs: 'FAQs',
+  settings: 'Settings',
+};
