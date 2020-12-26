348c5117f9cc9d074703502f7e600a950ec88169
iurybrasileiro <iury.brasileiro@outlook.com.br>
2020-12-25 21:03:34 -0300

    chore: define home structure

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,42 @@
 import React, { FC } from 'react';
-import { View } from 'react-native';
+import strings from '~/global/strings';
+
+import {
+  Container,
+  ListTitle,
+  AlbumsList,
+  AlbumContainer,
+  AlbumImage,
+  AlbumTitle,
+  AlbumArtistName,
+} from './styles';
 
 const Home: FC = () => {
-  return <View />;
+  return (
+    <Container>
+      <ListTitle>{strings.recommented_for_you}</ListTitle>
+      <AlbumsList
+        data={Array(4).fill('')}
+        renderItem={() => (
+          <AlbumContainer>
+            <AlbumImage />
+            <AlbumTitle>Monsters Go Bump</AlbumTitle>
+            <AlbumArtistName>ERIKA RECINOS</AlbumArtistName>
+          </AlbumContainer>
+        )}
+      />
+      <ListTitle>{strings.my_playlist}</ListTitle>
+      <AlbumsList
+        data={Array(4).fill('')}
+        renderItem={() => (
+          <AlbumContainer>
+            <AlbumImage />
+            <AlbumTitle>Monsters Go Bump</AlbumTitle>
+          </AlbumContainer>
+        )}
+      />
+    </Container>
+  );
 };
 
 export default Home;
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components/native';
+import { Dimensions, FlatList } from 'react-native';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+
+  padding: 8px 14px;
+`;
+
+export const ListTitle = styled.Text`
+  margin-left: 14px;
+  margin-bottom: 8px;
+
+  font-size: 24px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.secundary};
+`;
+
+export const AlbumsList = styled(FlatList as new () => FlatList<any>).attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  keyExtractor: (_, index) => String(index),
+})`
+  margin-bottom: 32px;
+`;
+
+export const AlbumContainer = styled.View`
+  width: ${Dimensions.get('window').width / 2}px;
+
+  border-radius: 4px;
+  margin: 0px 14px;
+
+  align-items: center;
+`;
+
+export const AlbumImage = styled.View`
+  width: 100%;
+  height: ${Dimensions.get('window').width / 2}px;
+
+  border-radius: 4px;
+  margin-bottom: 8px;
+
+  background: #7159c1;
+`;
+
+export const AlbumTitle = styled.Text`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.secundary};
+`;
+
+export const AlbumArtistName = styled.Text`
+  font-size: 10px;
+  color: ${({ theme }) => theme.colors.secundary};
+`;
